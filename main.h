ce9d6a98c7f062f5b9fa67086a629a04dc9e466d
Maggie-Ciku <margaret.wanjiku0883@gmail.com>
2022-07-11 00:48:20 +0300

    printf

--- main.h
+++ main.h
@@ -19,4 +19,9 @@ void reset_buffer(char buffer[]);
 void format_s(va_list valist, char *buffer, int *index);
 void format_c(va_list valist, char *buffer, int *index);
 void format_perc(va_list valist, char *buffer, int *index);
+char *_memcpy(char *dest, char *src, unsigned int n);
+void print_char(va_list valist);
+void print_int(va_list valist);
+void print_float(va_list valist);
+void print_string(va_list valist);
 #endif
