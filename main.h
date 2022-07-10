d48eb2261678137cbc3dff339fdfa563e0aa9cae
Maggie-Ciku <margaret.wanjiku0883@gmail.com>
2022-07-10 17:54:35 +0300

    printf

---
+++ main.h
@@ -0,0 +1,33 @@
+#ifndef MMAIN.H
+#define MAIN.H
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+typedef struct flags
+{
+	int plus;
+	int space;
+	int hash;
+}flags_t;
+/**
+ * struct printHandler - struct to choose the right function depending
+ * on the format specifier passed to _printf()
+ * @c: format specifier
+ * @f: pointer to the correct printing function
+ */
+typedef struct printHandler
+{
+	char c;
+	int (*f)(va_list ap, flags_t *f);
+}ph;
+
+int _putchar(char c);
+int _puts(char *str);
+int _printf(const char *format, ...);
+#endif
