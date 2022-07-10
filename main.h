c3480f4ec2e4242a4d1b4d5f9cb7779b9246b4b7
Maggie-Ciku <margaret.wanjiku0883@gmail.com>
2022-07-10 23:28:12 +0300

    printf

--- main.h
+++ main.h
@@ -4,34 +4,17 @@
 #include <stdarg.h>
 #include <stdio.h>
 /**
- * struct flags - struct containing flags to "turn on"
- * when a flag specifier is passed to _printf()
- * @plus: flag for the '+' character
- * @space: flag for the ' ' character
- * @hash: flag for the '#' character
+ * struct vtype - struct vtype
+ * @tp: tp
+ * @f: function
  */
-typedef struct flags
+typedef struct vtype
 {
-	int plus;
-	int space;
-	int hash;
-}flags_t;
-/**
- * struct printHandler - struct to choose the right function depending
- * on the format specifier passed to _printf()
- * @c: format specifier
- * @f: pointer to the correct printing function
- */
-typedef struct printHandler
-{
-	char c;
-	int (*f)(va_list ap, flags_t *f);
-}ph;
-
-int _putchar(char c);
-int _puts(char *str);
+	char tp;
+	void (*f)();
+}vtype_t;
 int _printf(const char *format, ...);
-int (*get_print(char s))(va_list, flags_t *);
-int get_flag(char s, flags_t *f);
+void _write_buffer(char *buffer, int *index);
+void reset_buffer(char buffer[]);
 
 #endif
