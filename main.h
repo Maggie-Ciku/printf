3e4a1b3f1aca0cee6d9025fb5ea892c635eb6d40
major0001 <alexmutune746@gmail.com>
2022-07-12 17:55:24 -0700

    doing it

--- main.h
+++ main.h
@@ -2,28 +2,7 @@
 #define MAIN_H
 
 #include <stdarg.h>
-#include <stdio.h>
-/**
- * struct print - structure for printing various types
- * @t: type to print
- * @f: function
- */
-typedef struct print
-{
-	char *t;
-	int (*f)(va_list);
-} print_t;
-int _putchar(char c);
-int _printf(const char *format, ...);
-int print_c(va_list c);
-int print_s(va_list s);
-int print_d(va_list d);
-int print_i(va_list i);
-int print_b(va_list b);
-int print_o(va_list o);
-int print_x(va_list x);
-int print_X(va_list X);
-int print_u(va_list u);
+
 int _putchar(char c);
 int print_int(va_list arg);
 int print_unsigned(va_list arg);
@@ -41,14 +20,14 @@ int print_HEX(va_list arg);
 int print_STR (va_list arg);
 
 /**
- *  * struct identifierStruct - structure definition of a printTypeStruct
- *   * @indentifier: type
- *    * @printer: function to print
- *     */
+ * struct identifierStruct - structure definition of a printTypeStruct
+ * @indentifier: type
+ * @printer: function to print
+ */
 typedef struct identifierStruct
 {
-	char *indentifier;
-	int (*printer)(va_list);
+char *indentifier;
+int (*printer)(va_list);
 } identifierStruct;
 
 #endif
